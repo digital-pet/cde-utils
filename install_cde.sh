5cf26377a359d85fe3d2f2a400e1acf433decbb6
Tama Gotchi <47679051+digital-pet@users.noreply.github.com>
2022-01-05 02:49:13 -0500

    Update install_cde.sh

--- install_cde.sh
+++ install_cde.sh
@@ -3,7 +3,7 @@
 echo "This script will install what are, in my opinion, the core components needed for CDE on a clean Sparky MinimalCLI install"
 
 sudo apt update
-sudo apt install cde-desktop x11-apps xinit picom anarchism xsettingsd stalonetray firefox pavucontrol
+sudo apt install cde-desktop x11-apps xinit picom network-manager-gnome anarchism xsettingsd stalonetray firefox pulseaudio pavucontol audacious pnmixer dunst
 
 # dtappgather needs the suid bit set in order to function properly - eventually this will be replaced but today is not that day
 sudo chmod u+s /usr/dt/bin/dtappgather
