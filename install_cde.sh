51afe64056066ba57d0c094939378c30d7b9c680
Tama Gotchi <47679051+digital-pet@users.noreply.github.com>
2022-01-04 23:50:16 -0500

    add suid bit to dtappgather

--- install_cde.sh
+++ install_cde.sh
@@ -4,3 +4,6 @@ echo "This script will install what are, in my opinion, the core components need
 
 sudo apt update
 sudo apt install cde-desktop x11-apps xinit picom anarchism xsettingsd stalonetray firefox
+
+# dtappgather needs the suid bit set in order to function properly - eventually this will be replaced but today is not that day
+sudo chmod u+s /usr/dt/bin/dtappgather
