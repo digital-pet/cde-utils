b95e08416b540081d6a16db6502bb44ebcc004da
Tama Gotchi <47679051+digital-pet@users.noreply.github.com>
2022-01-04 21:12:45 -0500

    First revision

---
+++ install_cde.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "This script will install what are, in my opinion, the core components needed for CDE on a clean Sparky MinimalCLI install"
+
+sudo apt update
+sudo apt install cde-desktop x11-apps xinit picom anarchism xsettingsd stalonetray firefox
